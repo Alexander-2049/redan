e734c15041bce2ae9fabb343ab3bbced19d04770
Alexander2049 <a.alexander.2049@gmail.com>
2025-01-22 11:51:14 +0200

    Using custom hook

--- src/renderer/web/components/pages/debug.tsx
+++ src/renderer/web/components/pages/debug.tsx
@@ -1,38 +1,19 @@
-import { useState, useEffect, useMemo } from "react";
-
-import { WEBSOCKET_SERVER_PORT } from "../../../../shared/constants";
-import { GameAPI } from "../../utils/GameAPI";
-
-    // controls: new ConnectedListeners(),
-    // rpm: new ConnectedListeners(),
-    // speed: new ConnectedListeners(),
-    // "carLocation": new ConnectedListeners(),
+import { useGameAPIEvents } from "../../hooks/useGameAPIEvents";
 
 const Debug = () => {
-  const api = useMemo(() => new GameAPI(WEBSOCKET_SERVER_PORT), []);
-  const [controls, setControls] = useState(null);
-  const [rpm, setRpm] = useState(null);
-  const [speed, setSpeed] = useState(null);
-  const [carLocation, setCarLocation] = useState(null);
-
-  useEffect(() => {
-    api.addEventListener("controls", setControls);
-    api.addEventListener("rpm", setRpm);
-    api.addEventListener("speed", setSpeed);
-    api.addEventListener("carLocation", setCarLocation);
-
-    return () => {
-      api.removeEventListener("controls", setControls);
-      api.removeEventListener("rpm", setControls);
-      api.removeEventListener("speed", setControls);
-      api.removeEventListener("carLocation", setControls);
-    };
-  }, []);
+  const { controls, rpm, speed, carLocation, gear } = useGameAPIEvents([
+    "controls",
+    "rpm",
+    "speed",
+    "carLocation",
+    "gear",
+  ]);
 
   return (
     <div>
       <pre>{JSON.stringify(controls, undefined, " ")}</pre>
       <pre>{JSON.stringify(rpm, undefined, " ")}</pre>
+      <pre>{JSON.stringify(gear, undefined, " ")}</pre>
       <pre>{JSON.stringify(speed, undefined, " ")}</pre>
       <pre>{JSON.stringify(carLocation, undefined, " ")}</pre>
     </div>
