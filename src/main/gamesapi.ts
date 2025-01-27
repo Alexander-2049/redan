f41ce4874b483fd38f154dfed33fca8502d5577b
Alexander-2049 <a.alexander.2049@gmail.com>
2025-01-28 01:53:32 +0200

    Telemetry and sessionInfo are both saved in a separate object

--- src/main/gamesapi.ts
+++ src/main/gamesapi.ts
@@ -1,24 +1,39 @@
+import { TelemetryInterface } from "../shared/types/telemetry";
 import { iRacingSDK } from "./games/iracing/iRacingSDK";
 import { GamesWebSocketServerAPI } from "./models/GamesWebSocketServerAPI";
 import { formatTelemetryData } from "./utils/formatTelemetryData";
+import { SessionInfo } from "../shared/types/sessionInfo";
 
 export const irsdkipc = new iRacingSDK();
 
 export const gamesWebSocketServerAPI = new GamesWebSocketServerAPI();
 
-// irsdkipc.on("sessionInfo", (sessionInfo) => {
-//   if (gamesWebSocketServerAPI.getSelectedGame() !== "IRACING") return;
-// });
+const iracingData: {
+  telemetry: TelemetryInterface;
+  sessionInfo: SessionInfo;
+} = {
+  telemetry: null,
+  sessionInfo: null,
+};
+
+irsdkipc.on("sessionInfo", (sessionInfo) => {
+  if (gamesWebSocketServerAPI.getSelectedGame() !== "IRACING") return;
+  iracingData.sessionInfo = sessionInfo.data;
+
+  gamesWebSocketServerAPI.groups.sessionInfo.send(sessionInfo.data);
+});
 
 irsdkipc.on("telemetry", (telemetry) => {
   if (gamesWebSocketServerAPI.getSelectedGame() !== "IRACING") return;
+  iracingData.telemetry = telemetry.data;
 
   const formattedTelemetryData = formatTelemetryData(telemetry.data);
 
+  gamesWebSocketServerAPI.groups.telemetry.send(telemetry.data);
   gamesWebSocketServerAPI.groups.rpm.send(formattedTelemetryData.rpm);
   gamesWebSocketServerAPI.groups.controls.send(formattedTelemetryData.controls);
   gamesWebSocketServerAPI.groups.speed.send(formattedTelemetryData.speed);
-  gamesWebSocketServerAPI.groups.carLocation.send(formattedTelemetryData.carLocation);
+  gamesWebSocketServerAPI.groups.state.send(formattedTelemetryData.state);
 });
 
 // TODO ??
