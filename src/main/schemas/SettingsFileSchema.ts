7cc32e8061b226320d3c463cbef60fe83e3ae1e4
Alexander-2049 <a.alexander.2049@gmail.com>
2025-02-09 22:06:19 +0200

    Added zod library

---
+++ src/main/schemas/SettingsFileSchema.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+const versionRegex = /^\d+\.\d+(\.\d+)?$/;
+
+export const OverlaySettingsSchema = z.object({
+  folderName: z.string(),
+  windowWidth: z.number(),
+  windowHeight: z.number(),
+  windowPositionX: z.number(),
+  windowPositionY: z.number(),
+  clickThrough: z.boolean(),
+  resizable: z.boolean(),
+  draggable: z.boolean(),
+});
+
+export const SettingsFileSchema = z.object({
+  version: z.string().regex(versionRegex, "Invalid version format"),
+  overlays: z.array(OverlaySettingsSchema),
+});
--- src/main/utils/overlaysFolder.ts
+++ src/main/utils/overlaysFolder.ts
@@ -1,6 +1,14 @@
 import { OVERLAYS_PATH, OVERLAYS_SETTINGS_FILE_PATH } from "../constants";
 import fs from "fs";
 import path from "path";
+import {
+  OverlaySettingsSchema,
+  SettingsFileSchema,
+} from "../schemas/SettingsFileSchema";
+import { z } from "zod";
+
+export type OverlaySettings = z.infer<typeof OverlaySettingsSchema>;
+export type SettingsFile = z.infer<typeof SettingsFileSchema>;
 
 export const createOverlaysFolder = () => {
   if (!fs.existsSync(OVERLAYS_PATH)) {
@@ -13,29 +21,12 @@ export const createOverlaysFolder = () => {
 
 export const getOverlayNames = () => {
   const dir = fs.readdirSync(OVERLAYS_PATH);
-  const overlayNames: string[] = [];
-  for (let i = 0; i < dir.length; i++) {
-    if (fs.statSync(path.join(OVERLAYS_PATH, dir[i])).isDirectory())
-      overlayNames.push(dir[i]);
-  }
-  return overlayNames;
+  return dir.filter((item) =>
+    fs.statSync(path.join(OVERLAYS_PATH, item)).isDirectory()
+  );
 };
 
-export interface OverlaySettingsInterface {
-  folderName: string;
-  windowWidth: number;
-  windowHeight: number;
-  windowPositionX: number;
-  windowPositionY: number;
-  clickThrough: boolean;
-  resizable: boolean;
-  draggable: boolean;
-}
-
-const settingsFileTemplate: {
-  version: string;
-  overlays: OverlaySettingsInterface[];
-} = {
+const settingsFileTemplate = {
   version: "1.0",
   overlays: [],
 };
@@ -46,5 +37,25 @@ export const createOverlaySettingsFile = () => {
       OVERLAYS_SETTINGS_FILE_PATH,
       JSON.stringify(settingsFileTemplate, null, 4)
     );
+  }
+};
+
+export const readOverlaySettingsFile = () => {
+  if (!fs.existsSync(OVERLAYS_SETTINGS_FILE_PATH)) {
+    createOverlaySettingsFile();
+  }
+
+  const fileContent = fs.readFileSync(OVERLAYS_SETTINGS_FILE_PATH, "utf-8");
+  try {
+    const parsedData = JSON.parse(fileContent);
+    const result = SettingsFileSchema.safeParse(parsedData);
+    if (!result.success) {
+      console.error("Invalid settings file format:", result.error.format());
+      return null;
+    }
+    return result.data;
+  } catch (error) {
+    console.error("Error reading settings file:", error);
+    return null;
   }
 };
