0405c8a06dbe29c10228c4d2f9629297bbb8d6a1
Alexander2049 <a.alexander.2049@gmail.com>
2025-03-27 16:21:06 +0200

    Add connection handler to WebSocket server for logging request URLs

--- src/main/services/gameWebSocketServer/GameWebSocketServer.ts
+++ src/main/services/gameWebSocketServer/GameWebSocketServer.ts
@@ -1,8 +1,14 @@
 import { WEBSOCKET_SERVER_PORT } from "src/shared/constants";
-import { WebSocket } from "ws";
+import WebSocket from "ws";
 
 export class GameWebSocketServer {
   private wss = new WebSocket.Server({
     port: WEBSOCKET_SERVER_PORT,
   });
+
+  constructor() {
+    this.wss.on("connection", (socket, request) => {
+      console.log(request.url);
+    });
+  }
 }
