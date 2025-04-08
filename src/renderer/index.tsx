bdf50949c9bb0e820aa2bbc5790e15900487f9b4
Alexander2049 <a.alexander.2049@gmail.com>
2025-04-08 14:04:48 +0300

    Refactor index.html and index.tsx for improved structure; add DebugPage component for WebSocket data handling

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,35 +1,24 @@
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
 import "./i18n";
-import useWebSocket from "./hooks/useWebSocket";
-import { useEffect } from "react";
 import TitleBar from "./components/TitleBar";
+import DebugPage from "./pages/DebugPage";
 
 const Main = () => {
-  const { t } = useTranslation();
-
-  const { data, error } = useWebSocket("ws://localhost:49791", [
-    "realtime.throttle",
-    "realtime.brake",
-  ]);
-
-  useEffect(() => {
-    if (error) {
-      console.error("WebSocket Error:", error);
-    }
-
-    console.log("WebSocket Data:", data);
-  }, [data, error]);
+  // const { t } = useTranslation();
+  /*
+        The title is: {t("title")}
+        Description: {t("description.part1")}
+  */
 
   return (
-    <>
+    <div className="flex h-full flex-col">
       <TitleBar />
-      The title is: {t("title")}
-      Description: {t("description.part1")}
-      <br />
-      <pre>
-        <code>{JSON.stringify(data, null, "  ")}</code>
-      </pre>
-    </>
+      <div className="grow">
+        {/* Content START */}
+        <DebugPage />
+        {/* Content END */}
+      </div>
+    </div>
   );
 };
 
---
+++ src/renderer/pages/DebugPage.tsx
@@ -0,0 +1,27 @@
+import useWebSocket from "../hooks/useWebSocket";
+import { useEffect } from "react";
+
+const DebugPage = () => {
+  const { data, error } = useWebSocket("ws://localhost:49791", [
+    "realtime.throttle",
+    "realtime.brake",
+  ]);
+
+  useEffect(() => {
+    if (error) {
+      console.error("WebSocket Error:", error);
+    }
+
+    console.log("WebSocket Data:", data);
+  }, [data, error]);
+
+  return (
+    <div>
+      <pre>
+        <code>{JSON.stringify(data, null, "  ")}</code>
+      </pre>
+    </div>
+  );
+};
+
+export default DebugPage;
