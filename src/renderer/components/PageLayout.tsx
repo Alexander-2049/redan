49ee9ff5921e1676ecda565f82ab74c3e5566379
Alexander-2049 <a.alexander.2049@gmail.com>
2025-04-09 19:04:34 +0300

    feat: replace div with ScrollArea in PageLayout for improved scrolling behavior

--- src/renderer/components/PageLayout.tsx
+++ src/renderer/components/PageLayout.tsx
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
+import { ScrollArea } from "./ui/scroll-area";
 
 const PageLayout = () => {
   return (
@@ -9,9 +10,9 @@ const PageLayout = () => {
       <div className="flex h-0 grow flex-col">
         <div className="flex h-full grow flex-row">
           <Sidebar />
-          <div className="grow overflow-hidden">
+          <ScrollArea className="overflow-y h-full grow bg-[#f5f5f5]">
             <Outlet />
-          </div>
+          </ScrollArea>
         </div>
       </div>
     </>
