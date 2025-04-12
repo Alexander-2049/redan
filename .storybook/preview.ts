df9da0795e19b6fced3d3e1c805dd70fa46483d6
Alexander-2049 <a.alexander.2049@gmail.com>
2025-04-12 14:32:25 +0300

    fix: update Tailwind CSS import path in Storybook preview configuration for Tailwind v4

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,5 @@
 import type { Preview } from "@storybook/react";
-import "!style-loader!css-loader!postcss-loader!tailwindcss/tailwind.css";
+import "!style-loader!css-loader!postcss-loader!tailwindcss/index.css";
 
 const preview: Preview = {
   parameters: {
