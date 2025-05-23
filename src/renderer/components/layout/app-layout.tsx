02ae78493713f567f6a2207ce18473253d996e26
Alexander-2049 <a.alexander.2049@gmail.com>
2025-05-23 21:41:04 +0300

    refactor: remove Header component from layout

--- src/renderer/components/layout/app-layout.tsx
+++ src/renderer/components/layout/app-layout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
 import TitleBar from "../title-bar";
-import Header from "../header";
 
 function Layout() {
   return (
     <div className="grid h-full grid-rows-[auto_auto_1fr] overflow-hidden">
       <TitleBar />
-      <Header />
       <div className="overflow-hidden">
         <Outlet />
       </div>
