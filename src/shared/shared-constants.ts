0a8e71335bcf37e607254abd28a826a8d4c535d0
Alexander-2049 <a.alexander.2049@gmail.com>
2025-06-08 22:46:35 +0300

    feat: add ASSETS_URL constant for asset server URL configuration

--- src/shared/shared-constants.ts
+++ src/shared/shared-constants.ts
@@ -2,3 +2,4 @@ export const WEBSOCKET_SERVER_PORT = 49791;
 export const OVERLAY_SERVER_PORT = 48792;
 export const ASSETS_SERVER_PORT = 48793;
 export const WEBSOCKET_SERVER_DEMO_PORT = 49794;
+export const ASSETS_URL = `http://localhost:${ASSETS_SERVER_PORT}`;
