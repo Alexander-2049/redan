ac1c03d3a11fedfe2de6f77f01e110eab7eb114e
Alexander-2049 <a.alexander.2049@gmail.com>
2025-06-14 01:41:40 +0300

    fix: conditionally render layout components based on loading state

--- src/renderer/routes/my-layouts-route.tsx
+++ src/renderer/routes/my-layouts-route.tsx
@@ -1,13 +1,22 @@
+import { useLayouts } from "../api/layouts/get-layouts";
+import { useOverlays } from "../api/overlays/get-overlays";
 import LayoutList from "../components/my-layouts/layout-list";
 import LayoutSettings from "../components/my-layouts/layout-settings";
 import OverlaysList from "../components/my-layouts/overlays-list";
 
 const MyLayoutsRoute = () => {
+  const layouts = useLayouts();
+  const overlays = useOverlays();
+
   return (
     <div className="grid h-full grid-cols-[auto_1fr_auto]">
-      <LayoutList />
-      <OverlaysList />
-      <LayoutSettings />
+      {layouts.isLoading || overlays.isLoading ? null : (
+        <>
+          <LayoutList />
+          <OverlaysList />
+          <LayoutSettings />
+        </>
+      )}
     </div>
   );
 };
